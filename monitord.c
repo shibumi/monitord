00c9f5f2fac9576b6475b50cc997f7e1265e5040
Christian Rebischke <Chris.Rebischke@posteo.de>
2016-06-09 00:12:24 +0200

    first version of disk_space

--- monitord.c
+++ monitord.c
@@ -27,24 +27,51 @@
 */
 
 #define TIME_BUFFER_SIZE 26
+#define FILE_PATH_SIZE 1024
+#define DISK_PATH "/"
+#define MAGIC_NUMBER 1073741824
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <X11/Xlib.h>
 #include <systemd/sd-bus.h>
+#include <sys/statvfs.h>
+#include <math.h>
 
-
-char* getDatetime(){
+char* datetime(){
         time_t timer;
         struct tm* time_info;
         char* buffer = malloc(TIME_BUFFER_SIZE);
         
         time(&timer);
         time_info = localtime(&timer);
-        strftime(buffer, TIME_BUFFER_SIZE, "%Y-%m-%d %I:%M %p", time_info);        
+        strftime(buffer, TIME_BUFFER_SIZE, "%Y-%m-%d %I:%M %p", time_info);
+        free(buffer);      
         return buffer;
 }
 
+char* disk_space(){
+        unsigned long total, used, blocksize, free;
+        unsigned long usedP;
+        struct statvfs vfs;
+        char* buffer = malloc(FILE_PATH_SIZE);
+
+        if(statvfs(DISK_PATH, &vfs) != 0){
+                fprintf(stderr, "Couldn't stat mountpoint %s for disk space\n");
+        } else {
+                blocksize = vfs.f_bsize;
+                total = vfs.f_blocks * blocksize;
+                free = (vfs.f_bfree * blocksize);
+                used = ceil((double)(total - free) / MAGIC_NUMBER);
+                total = ceil((double)(total) / MAGIC_NUMBER);
+                usedP = (used / total) * 100;
+                printf("%lu\n", usedP);
+                printf("D: %lu / %lu\n", used, total);
+        }
+        return "";
+}
+
 int main(){
+        disk_space();
         return 0;
 }
